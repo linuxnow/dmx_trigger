d4b85b98915a5a6423996b04f7ee9daff16d7a0f
Pau Aliagas <pau@aliagas.net>
2021-03-07 12:34:26 +0100

    Make a python package.

---
+++ README.md
@@ -0,0 +1,8 @@
+# DMX Video trigger package
+
+There are two classes:
+-a DMX monitor
+-a video provider
+
+You provide callback functions to the video provider so tht they can be
+triggered when DMX data arrives.
---
+++ _init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+"""
+DMX Video Trigger
+"""
+
+__author__ = "Pau Aliagas <pau@newtral.org>"
+__copyright__ = "Copyright (c) 2021 Pau Aliagas"
+__license__ = "GPL 3.0"
+__version__ = "0.1"
+__url__ = "http://www.newtral.org"
+__all__ = ['config', 'settings', 'ola_dmx_video_trigger']
+
