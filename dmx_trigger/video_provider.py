633e89ae3672b1358463cdddeb257da86edc01d1
Pau Aliagas <pau@aliagas.net>
2021-03-19 14:14:39 +0100

    Jump to version 0.1.3.
    -Move to playlist
     * use MediaListPlayer instead of MediaPlayer,
     * this allows us to set a loop on the playlist which cannot be done
       with single files.
     * we create a single element playlist each time, it's the only
       workaround possible to loop on single files.
    
    -Init vlc in a separate method.
    
    -Cache checked media in a fast-access dict indexed with (theme, scene) keys.

--- dmx_trigger/video_provider.py
+++ dmx_trigger/video_provider.py
@@ -24,7 +24,8 @@ DELTA_RATE=0.05
 class VLCVideoProviderDir(object):
     def __init__(self, media_config=None, file_ext=valid_extensions, volume=0):
         self._media_config = media_config
-        self._playlist = media_config['playlist']
+        self._playlist = media_config["playlist"]
+        self._vlclist = {}
         self._file_ext = file_ext
         self.current_theme = None
         self.requested_theme = 0
@@ -34,66 +35,93 @@ class VLCVideoProviderDir(object):
         self.requested_reset_rate = False
         self._rewind = False
         self._volume = volume
+        self.vlc = {
+            "instance": None,
+            "player": None,
+            "list_player": None,
+            "playlist": None,
+        }
 
         # vlc player
-        self.media_player = vlc.MediaPlayer("--fullscreen ", "--no-audio", "--intf", "'--mouse-hide-timeout=0", "--video-on-top")
-        self.media_player.set_fullscreen(True)
-        self.media_player.audio_set_volume(self._volume)
+        self._init_vlc()
+        # indexed access list to file names and properties
+        self._vlclist = self._build_playlist_from_config()
 
-        # read media file config
-        if self._media_config:
-            self._check_files_from_config()
+    def _init_vlc(self):
+        """
+        Initialise the VLC variables:
+            - The VLC instance
+            - A MediaListPlayer for playing playlists
+            - A MediaPlayer for controlling playback
+            - A MediaList to load in the MediaListPlayer
+        Documentation for these can be found here:
+            http://www.olivieraubert.net/vlc/python-ctypes/doc/
+        """
+        # vlc media list player
+        flags = ["--quiet", "--no-audio", "--intf", "--mouse-hide-timeout=0", "--video-on-top", "volume={}".format(self._volume)]
+        self.vlc["instance"] = vlc.Instance(flags)
+        self.vlc['list_player'] = self.vlc['instance'].media_list_player_new()
+        self.vlc['player'] = self.vlc['list_player'].get_media_player()
+        self.vlc["player"].set_fullscreen(True)
+        self.vlc['playlist'] = self.vlc['instance'].media_list_new()
+        self.vlc["list_player"].set_media_list(self.vlc["playlist"])
 
-    def _get_filename(self, n, scene=0):
+    def _get_filename(self, theme, scene=0):
         """Get the full path filename.
 
-        :param int n: theme number
+        :param int theme: theme number
         :param int scene: scene number
         :rtype str
         """
-        logger.debug("Get filename in position {}.{}".format(n, scene))
+        logger.debug("Get filename in position {}.{}".format(theme, scene))
         try:
-            dir = self._playlist[n]['dir']
-            files = self._playlist[n]['files']
+            logger.debug("vlclist item {}.{}: {}".format(theme, scene, self._vlclist[theme, scene]))
+            return self._vlclist[theme, scene]["file"]
         except KeyError:
-            msg = "No 'dirs' or 'files' section in position {}.{}".format(n, scene)
+            msg = "No files in position {}.{}".format(theme, scene)
             logger.warn(msg)
             raise Exception(msg)
-        try:
-            f = files[scene]
-        except IndexError:
-            msg = "No file in position {}.{}".format(n, scene)
-            logger.warn(msg)
-            raise Exception(msg)
-        file = os.path.join(dir, f)
-        logger.debug("File {} in pos {}.{}".format(file, n, scene))
-        return file
 
-    def _check_files_from_config(self):
+    def _build_playlist_from_config(self, media_list = None):
         """
         This function is responsible for checking the media list.
 
-        IT reads a yaml configuration file.
+        It reads a yaml configuration file and adds all valid entries
+        to the returned playlist.
+
+        :rtype dict
         """
         logger.debug("Check load file list from config")
+        vlclist = {}
+        pos = 0
         for p in self._playlist:
-            dir = self._playlist[p]['dir']
-            files = self._playlist[p]['files']
+            dir = self._playlist[p]["dir"]
+            files = self._playlist[p]["files"]
+            try:
+                playmode = self._playlist[p]["playmode"]
+            except KeyError:
+                playmode = False
             for (idx, f) in enumerate(files):
                 file = os.path.join(dir, f)
                 if os.path.isfile(file):
                     logger.debug("File {} in pos {}.{} exists".format(file, p, idx))
                     if os.path.splitext(f)[1] not in self._file_ext:
                         logger.warn("File {} in pos {}.{} does not have a valid extension".format(file, p, idx))
+                    vlclist[p, idx] = {"file": file, "playmode": playmode, "pos": pos}
+                    if media_list:
+                        media_list.add_media(vlc.Media(file))
+                    pos += 1
+                    logger.debug("vlclist item {}.{}: {}".format(p, idx, vlclist[p, idx]))
                 else:
                     logger.warn("File {} in pos {}.{} does not exist".format(file, p, idx))
+        return vlclist
 
-    def _load_media(self, file, reset_rate=True):
+    def _load_media(self, file, playmode="default"):
         """Loads media
 
         Loads the requested media.
 
-        Returns True if all could be executted successfully, False otherwise.
+        Returns True if all could be executed successfully, False otherwise.
 
         :rtype bool
         """
@@ -107,38 +135,120 @@ class VLCVideoProviderDir(object):
         # create a media object
         media = vlc.Media(file)
         # set media to the media player
-        self.media_player.set_media(media)
+        self.vlc["player"].set_media(media)
         return True
 
     def _play_media(self):
         """Play media unconditionally.
 
         Loads and plays the requested media, resetting all the properties that
-        need not be ececuted.
+        need not be executed.
 
-        Returns True if all could be executted successfully, False otherwise.
+        Returns True if all could be executed successfully, False otherwise.
 
         :rtype bool
         """
         logger.debug("Media play requested: {}.{}".format(self.requested_theme, self.requested_scene))
         try:
             file = self._get_filename(self.requested_theme, scene=self.requested_scene)
+            playmode = self._vlclist[self.requested_theme, self.requested_scene]["playmode"]
         except:
             msg = "Error getting file name in position {}.{}".format(self.requested_theme, self.requested_scene)
             logger.error(msg)
             return False
+
         # load_media
-        if self._load_media(file):
+        if self._load_media(file, playmode=playmode):
             # update current video
             self.current_theme = self.requested_theme
             self.current_scenee = self.requested_scene
             self.current_rate = self.requested_rate
             self._rewind = False
             # reset rate
-            self.media_player.set_rate(DEFAULT_RATE)
+            self.vlc["player"].set_rate(DEFAULT_RATE)
             # start playing video
             logger.debug("Play video {} in position {}.{}".format(file, self.requested_theme, self.requested_scene))
-            self.media_player.play()
+            self.vlc["player"].play()
+            return True
+        else:
+            logger.debug("Could not start video {} in position {}.{}".format(file, self.requested_theme, self.requested_scene))
+            return False
+
+    def _load_medialist(self, file, playmode="default"):
+        """Loads media
+
+        Loads the requested media list with a single file.
+
+        Returns True if all could be executed successfully, False otherwise.
+
+        :rtype bool
+        """
+        logger.debug("Media list load requested: {}, playmode = {}".format(file, playmode))
+
+        # check that file exists
+        if not os.path.isfile(file):
+            logger.warning("Video not found: {}".format(file))
+            return False
+
+        # create media list with one file
+        # first empty list
+        i = self.vlc["playlist"].count()
+        while i:
+            self.vlc["playlist"].remove_index(0)
+            i -= 1;
+        # create a media object
+        media = self.vlc['instance'].media_new(file)
+        # add media to the playlist
+        self.vlc["playlist"].add_media(media)
+        if playmode == "loop":
+            self.vlc["list_player"].set_playback_mode(vlc.PlaybackMode.loop)
+        elif playmode == "repeat":
+            self.vlc["list_player"].set_playback_mode(vlc.PlaybackMode.repeat)
+        else:
+            self.vlc["list_player"].set_playback_mode(vlc.PlaybackMode.default)
+
+        return True
+
+    def _play_medialist(self):
+        """Play media unconditionally from playlist.
+
+        Loads and plays the requested media, resetting all the properties that
+        need not be executed.
+
+        Returns True if all could be executed successfully, False otherwise.
+
+        :rtype bool
+        """
+        logger.debug("Media play requested: {}.{}".format(self.requested_theme, self.requested_scene))
+        try:
+            pos = self._vlclist[self.requested_theme, self.requested_scene]["pos"]
+            file = self._get_filename(self.requested_theme, scene=self.requested_scene)
+            playmode = self._vlclist[self.requested_theme, self.requested_scene]["playmode"]
+            logger.debug("Media play requested: {}.{} is in playlist position {}".format(self.requested_theme, self.requested_scene, pos))
+        except:
+            msg = "Error getting file name in position {}.{}".format(self.requested_theme, self.requested_scene)
+            logger.error(msg)
+            return False
+
+        # load_media
+        if self._load_medialist(file, playmode=playmode):
+            """
+            logger.debug("Playing list item {}".format(pos))
+            logger.debug("Ready to play list item {}".format(pos))
+            logger.debug("Media list has {} elements.".format(self.vlc["playlist"].count()))
+            logger.debug("Media item 0: {}".format(self.vlc["playlist"].item_at_index(0)))
+            """
+            # update current video
+            self.current_theme = self.requested_theme
+            self.current_scenee = self.requested_scene
+            self.current_rate = self.requested_rate
+            self._rewind = False
+            # reset rate
+            self.vlc["player"].set_rate(DEFAULT_RATE)
+            # start playing video
+            logger.debug("Play video {} in position {}.{}".format(file, self.requested_theme, self.requested_scene))
+            # we use next as a workaround otherwise the current media keeps plaiying
+            self.vlc["list_player"].next()
             return True
         else:
             logger.debug("Could not start video {} in position {}.{}".format(file, self.requested_theme, self.requested_scene))
@@ -148,13 +258,13 @@ class VLCVideoProviderDir(object):
         """Execute the delta rate change.
         """
         logger.debug("Change delta rate")
-        new_rate = rate = self.media_player.get_rate()
+        new_rate = rate = self.vlc["player"].get_rate()
         # change rate
         if self.current_rate > self.requested_rate:
             new_rate = rate - DELTA_RATE
         elif self.current_rate < self.requested_rate:
             new_rate = rate + DELTA_RATE
-        self.media_player.set_rate(new_rate)
+        self.vlc["player"].set_rate(new_rate)
         # update current rate
         self.current_rate = self.requested_rate
 
@@ -163,7 +273,7 @@ class VLCVideoProviderDir(object):
     def exec_pending(self):
         """Execute the pending actions.
 
-        Returns True if all could be executted successfully, False otherwise.
+        Returns True if all could be executed successfully, False otherwise.
 
         :rtype bool
         """
@@ -171,14 +281,14 @@ class VLCVideoProviderDir(object):
         # check for new thene and scene or rewind
         if (self.requested_theme != self.current_theme or
             self.requested_scene != self.current_scenee):
-            return self._play_media()
+            return self._play_medialist()
          # check for rewind
         elif self._rewind:
-            return self._play_media()
+            return self._play_medialist()
         # check for rate reset
         elif self.requested_reset_rate:
             # update current rate
-            self.media_player.set_rate(DEFAULT_RATE)
+            self.vlc["player"].set_rate(DEFAULT_RATE)
             self.requested_reset_rate = False
         # check for rate change
         elif self.requested_rate != self.current_rate:
@@ -240,12 +350,12 @@ class VLCVideoProviderDir(object):
             self._rewind = 1
 
     def pause(self, n, current=None):
-        if self.media_player.is_playing():
+        if self.vlc["player"].is_playing():
             logger.info("Video pause requested {:d}".format(n))
         else:
             logger.info("Video unpause requested {:d}".format(n))
-        self.media_player.pause()
+        self.vlc["player"].pause()
 
     def resume(self, n, current=None):
         logger.info("Video resume requested {:d}".format(n))
-        self.media_player.play()
+        self.vlc["player"].play()
